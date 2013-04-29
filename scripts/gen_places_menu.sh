9e273ddf2a7b5ff67845cfce5bc5dccf5a5624f9
Thomas Lübking <thomas.luebking@gmail.com>
2013-04-29 23:46:46 +0200

    make gen_places trigger a re-read of the menu

--- scripts/gen_places_menu.sh
+++ scripts/gen_places_menu.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-SHELL_MENU="`kde4-config --path data`"
-SHELL_MENU=${SHELL_MENU%%:*}be.shell/MainMenu/Places.xml
+SHELL_PATH="`kde4-config --path data`"
+SHELL_PATH="${SHELL_PATH%%:*}be.shell"
+SHELL_MENU=$SHELL_PATH/MainMenu/Places.xml
 STREAM='<submenu><menu label="Places">'
 STREAM="${STREAM} `xmlstarlet sel -T -t -m "/xbel/bookmark[info/metadata/IsHidden='false']" -o '<action label="' -v title -o '" exec="' -c "string(@href)" -o '"/>' -n $HOME/.local/share/user-places.xbel`"
 STREAM="${STREAM} </menu></submenu>"
 echo $STREAM > $SHELL_MENU
+touch $SHELL_PATH/MainMenu.xml
