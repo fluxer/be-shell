1fe87d6afaa6b36ea47c9c46afb6f3758d0d79ed
Thomas Lübking <thomas.luebking@gmail.com>
2013-06-27 12:19:02 +0200

    unfinished patch part slipped through..

--- tasks.cpp
+++ tasks.cpp
@@ -762,7 +762,7 @@ BE::Task::update(const unsigned long *properties)
 
     if (props[0] & NET::WMIcon && iconName().isEmpty()) // if an icon was set, this is a sticky one
     {
-        QIcon icn = BE::Plugged::themeIcon(exe().isEmpty() ? myGroup : exe(), false, parentWidget());
+        QIcon icn = BE::Plugged::themeIcon(exe().isEmpty() ? myGroup : exe(), false/*, parentWidget()*/);
         if (icn.isNull() && id)
             icn.addPixmap( KWindowSystem::icon(id) );
         if (!icn.isNull())
