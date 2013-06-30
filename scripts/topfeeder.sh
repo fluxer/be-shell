e65e1b9ab160177f1a9ad9fca2dd0c1639d31c66
Thomas Lübking <thomas.luebking@gmail.com>
2013-06-30 12:26:12 +0200

    runtime top section detection

--- scripts/topfeeder.sh
+++ scripts/topfeeder.sh
@@ -3,8 +3,12 @@
 
 FIFO=resources
 
+TOPLINE="`top -b 2>/dev/null | head -n7 | tail -n1`"
+CMD_OFF=`echo "${TOPLINE%COMMAND*}" | wc -c`
+CPU_OFF=`echo "${TOPLINE% \%CPU*}" | wc -c`
+
 daemon_func() {
-    top -bd5 | cut -c1-11,42-51,62- | grep --line-buffered --no-group-separator -A4 '%MEM' > /tmp/`whoami`/be.shell/resources
+    top -bd5 | cut -c1-11,${CPU_OFF}-$((CPU_OFF+10)),${CMD_OFF}- | grep --line-buffered --no-group-separator -A4 '%MEM' > /tmp/`whoami`/be.shell/resources
 }
 
 source `dirname "$0"`/be.fifo.daemon
